4bb550a54fa11d66d11f32b48cd75ec81620bbf7
davidalves1 <alves.david@outlook.com>
2017-04-04 23:22:26 -0300

    Atualização

--- src/js/app.js
+++ src/js/app.js
@@ -14,10 +14,11 @@ datepicker_nav.flatpickr({
 	defaultDate: new Date()
 });
 
-var ajax = function(url, method, data, callback) { 
+let ajax = function(config, callback) { 
 	
-	var xhr = new XMLHttpRequest();
-	xhr.open(method, url, true);
+	const xhr = new XMLHttpRequest();
+	
+	xhr.open(config.method, config.url, true);
 	xhr.setRequestHeader('Content-Type', 'application/json');
 	xhr.onload = function (e) {
 		if (xhr.readyState === 4) {
@@ -30,17 +31,77 @@ var ajax = function(url, method, data, callback) {
 
 	xhr.onerror = function (e) {
 		callback({
-			status: xhr.statusText,
+			status: xhr.status,
 			response: xhr.statusText
 		});
 	};
 
-	xhr.send(data);
+	xhr.send(config.data);
 };
 
-ajax('https://api.github.com/users/davidalves1', 'GET', {}, function(data) {
-	if (data.status !== 200)
-		console.log('Error ' + data.status);
+const config = {
+	url: 'https://api.github.com/users/davidalves1',
+	method: 'GET',
+	data: {}
+}
+
+ajax(config, function(data) {
+	if (200 !== data.status)
+		console.log(`Error - status: ${data.status}, response: ${data.response}`);
 	else
 		console.log('Sucesso!');
 });
+
+let result = [
+	{
+		id: 1,
+		date: '2017-03-01T19:00:00',
+		description: 'Missa de Cinzas - Pe. Luismar'
+	},
+	{
+		id: 2,
+		date: '2017-03-05T19:00:00',
+		description: 'Missa de Quaresma - Pe. Malvino \n\n A única área que eu acho, que vai exigir muita atenção nossa, e aí eu já aventei a hipótese de até criar um ministério. É na área de... Na área... Eu diria assim, como uma espécie de analogia com o que acontece na área agrícola.'
+	}
+];
+
+let table = `
+	<table class="mt-2">
+		<thead>
+			<tr>
+				<th data-field="time">Hora</th>
+				<th data-field="description">Descrição</th>
+			</tr>
+		</thead>
+		<tbody>
+`;
+
+result.forEach(function(item, key) {
+	let time = formatTime(item.date);
+
+	table += `
+		<tr>
+			<td>${time}</td>
+			<td>${item.description}</td>
+		</tr>
+	`;
+});
+
+table += `</tbody></table>`;
+
+function formatTime(date) {
+	let new_date = new Date(date);
+
+	let hora = new_date.getHours();
+	hora = 1 === hora.toString().length ? '0' + hora : hora;
+
+	let minuto = new_date.getMinutes();
+	minuto = 1 === minuto.toString().length ? '0' + minuto : minuto;
+
+	let segundo = new_date.getSeconds();
+	segundo = 1 === segundo.toString().length ? '0' + segundo : segundo;
+
+	return `${hora}:${minuto}`;
+}
+
+document.querySelector('#lista-eventos').innerHTML = table;
